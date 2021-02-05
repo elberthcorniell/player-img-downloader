46dc33c2a7373138266cccade91655ced473949b
Elbert Corniell <elberthcorniell@gmail.com>
2021-02-04 20:07:32 -0400

    Create style.js

---
+++ src/styles/style.js
@@ -0,0 +1,16 @@
+export const rowMargin = {
+    margin: '20px 0',
+}
+
+export const cardBody = {
+    textAlign: 'center',
+    position: 'relative',
+    paddingBottom: 80,
+}
+
+export const downloadButton = {
+    position: 'absolute',
+    right: 20,
+    bottom: 20,
+    width: 200,
+}
